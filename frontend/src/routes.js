a08b9382a6f2f79b57df1eb2a273c89932ab6016
viveksingh-01 <viveksingh9301@gmail.com>
2022-02-27 12:16:22 +0530

    wip: add parent layout and implement nested-routing for admin

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,3 +1,4 @@
+import AdminLayout from './views/admin/AdminLayout.svelte';
 import LunchMenuAdmin from './views/admin/LunchMenuAdmin.svelte';
 import Home from './views/public/Home.svelte';
 import LunchMenuView from './views/public/LunchMenuView.svelte';
@@ -5,7 +6,11 @@ import LunchMenuView from './views/public/LunchMenuView.svelte';
 const routes = [
   { name: '/', component: Home },
   { name: '/lunch-menu', component: LunchMenuView },
-  { name: '/admin/manage-menus', component: LunchMenuAdmin },
+  {
+    name: '/admin/manage-menus',
+    component: AdminLayout,
+    nestedRoutes: [{ name: 'index', component: LunchMenuAdmin }],
+  },
 ];
 
 export { routes };
---
+++ frontend/src/views/admin/AdminLayout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Route } from 'svelte-router-spa';
+
+  export let currentRoute;
+</script>
+
+<h3>Admin Layout</h3>
+
+<Route {currentRoute} />
