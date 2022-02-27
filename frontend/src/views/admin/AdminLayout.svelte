571237bab035c260bcc6559f8e743e105030d6d8
viveksingh-01 <viveksingh9301@gmail.com>
2022-02-27 12:35:35 +0530

    feat: add user store to share data amongst child components for admin

--- frontend/src/views/admin/AdminLayout.svelte
+++ frontend/src/views/admin/AdminLayout.svelte
@@ -1,9 +1,25 @@
 <script>
+  import { onMount } from 'svelte';
   import { Route } from 'svelte-router-spa';
+  import { user } from './../../stores.js';
 
   export let currentRoute;
+  let initialized = false;
+
+  onMount(() => {
+    user.set({
+      name: 'Test User',
+      schoolName: 'Test School',
+    });
+    initialized = true;
+  });
 </script>
 
-<h3>Admin Layout</h3>
-
-<Route {currentRoute} />
+<main>
+  <h3>Admin Layout</h3>
+  <section>
+    {#if initialized}
+      <Route {currentRoute} />
+    {/if}
+  </section>
+</main>
--- frontend/src/views/admin/LunchMenuAdmin.svelte
+++ frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -1,1 +1,7 @@
-<h2>Lunch Menu Admin</h2>
+<script>
+  import { user } from '../../stores';
+</script>
+
+<div>
+  <h1>Lunch Menu Administration / {$user.schoolName}</h1>
+</div>
