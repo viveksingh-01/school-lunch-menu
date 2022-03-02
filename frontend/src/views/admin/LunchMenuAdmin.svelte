b492c0c2e64dfcc6c4762e0cd3c91470353ae721
viveksingh-01 <viveksingh9301@gmail.com>
2022-03-02 22:57:58 +0530

    feat: add breadcrumb navigation in lunchmenu screen for admin

--- frontend/src/views/admin/LunchMenuAdmin.svelte
+++ frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -3,5 +3,13 @@
 </script>
 
 <div>
-  <h1 class="title is-4">Lunch Menu Administration / {$user.schoolName}</h1>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/admin/manage-menus">Lunch Menu Administration</a></li>
+      <li class="is-active">
+        <a href="/#" aria-current="page">{$user.schoolName}</a>
+      </li>
+    </ul>
+  </nav>
 </div>
