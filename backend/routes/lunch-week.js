979e8ab1239349a43388b44a0e0a4e94dd50ab18
viveksingh-01 <viveksingh9301@gmail.com>
2022-03-06 15:52:15 +0530

    wip: add dynamic-route to fetch lunch-week data by ID

--- backend/routes/lunch-week.js
+++ backend/routes/lunch-week.js
@@ -23,4 +23,14 @@ router.get('/', (req, res) => {
   res.status(200).send(lunchWeekList);
 });
 
+router.get('/:lunchWeekId', (req, res) => {
+  const id = parseInt(req.params.lunchWeekId);
+  const lunchWeek = lunchWeekList.find((e) => e.lunchWeekId === id);
+  console.log(lunchWeek);
+  if (!lunchWeek) {
+    return res.status(404).send('Requested data is not available.');
+  }
+  return res.status(200).send(lunchWeek);
+});
+
 module.exports = router;
